7e907d1011e2cc80912079ef491f7592d2513f29
Kayla Sween <kaylasween@Kaylas-MBP.sween.home>
2019-04-21 12:40:51 -0500

    adding contact page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,11 @@
 import React from "react"
+import Header from "../components/header"
+import { Link } from "gatsby"
 
 export default () => (
   <div style={{color: 'green'}}>
-    <h1>Hello Gatsby!</h1>
+    <Header headerText="Hello Gatsby!" />
+    <Link linkTo="/contact/">Contact</Link>
     <p>My first Gatsby page.</p>
     <img src="https://source.unsplash.com/random/400x200" alt="" />
   </div>
