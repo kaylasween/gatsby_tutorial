6884f8758e08f113db6e4219fa15c34c9618dcaa
Kayla Sween <kaylasween@Kaylas-MBP.sween.home>
2019-04-21 13:09:20 -0500

    linking index and contact pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import { Link } from "gatsby"
 export default () => (
   <div style={{color: 'green'}}>
     <Header headerText="Hello Gatsby!" />
-    <Link linkTo="/contact/">Contact</Link>
+    <Link to="/contact/">Contact</Link>
     <p>My first Gatsby page.</p>
     <img src="https://source.unsplash.com/random/400x200" alt="" />
   </div>
