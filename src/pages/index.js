bb37d684e03cfbbc70041f28d33723ce8d44ef9b
Kayla Sween <kaylasween@Kaylas-MBP.sween.home>
2019-04-21 12:05:18 -0500

    using proper React components. Now with props

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,9 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <div style={{color: 'green'}}>
+    <h1>Hello Gatsby!</h1>
+    <p>My first Gatsby page.</p>
+    <img src="https://source.unsplash.com/random/400x200" alt="" />
+  </div>
+)
